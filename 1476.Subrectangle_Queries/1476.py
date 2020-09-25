7d93987c5223f00e0faaf4cd8c9c29d58cea1a75
danbo06 <tanmasaki@gmail.com>
2020-09-26 08:59:09 +0900

    fix

--- 1476.Subrectangle_Queries/1476.py
+++ 1476.Subrectangle_Queries/1476.py
@@ -14,6 +14,21 @@ class SubrectangleQueries:
         return self.rectangle[row][col]
 
 
+
+    def __init__(self, rectangle: List[List[int]]):
+        # self.rectangleとrectangleが同じメモリを共有しないように__init__でrectangleをdeepcopyする
+        self.rectangle = copy.deepcopy(rectangle)
+        self.histories = []
+
+    def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+        self.histories.append([row1, col1, row2, col2, newValue])
+
+    def getValue(self, row: int, col: int) -> int:
+        for his in reversed(self.histories):
+            if his[0] <= row <= his[2] and his[1] <= col <= his[3]:
+                return his[4]
+        return self.rectangle[row][col]
+
 # Your SubrectangleQueries object will be instantiated and called as such:
 # obj = SubrectangleQueries(rectangle)
 # obj.updateSubrectangle(row1,col1,row2,col2,newValue)
