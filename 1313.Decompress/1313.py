00e868e0c0db1c6687ae65a7f54c9da615dcbfec
danbo06 <tanmasaki@gmail.com>
2020-09-29 20:53:48 +0900

    add 1line solution

--- 1313.Decompress/1313.py
+++ 1313.Decompress/1313.py
@@ -9,3 +9,8 @@ class Solution:
             for j in range(freq):
                 ans.append(val)
         return ans
+
+
+# one line solution
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        return [nums[i + 1] for i in range(0, len(nums), 2) for _ in range(nums[i])]
