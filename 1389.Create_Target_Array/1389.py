482f309e99d79116c4aaa4ebfe983fb6da3f28bd
danbo06 <tanmasaki@gmail.com>
2020-09-30 19:47:07 +0900

    add solutin

--- 1389.Create_Target_Array/1389.py
+++ 1389.Create_Target_Array/1389.py
@@ -6,3 +6,11 @@ class Solution:
         for i in range(len(nums)):
             ans.insert(index[i], nums[i])
         return ans
+
+# use zip 
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        ans = []
+        for i, v in zip(index, nums):
+            ans.insert(i, v)
+        return ans   
