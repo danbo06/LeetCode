a990ce44a81eea3d0dabcc2ff613ca28e4c1cb23
danbo06 <tanmasaki@gmail.com>
2020-10-01 22:57:42 +0900

    add 1486

---
+++ 1486.XOR_Operation/1486.py
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/xor-operation-in-an-array/
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums = []
+        ans = 0
+        for n in range(n):
+            nums.append(start + n * 2)
+        for n in nums:
+            ans = ans ^ n
+        return ans 
