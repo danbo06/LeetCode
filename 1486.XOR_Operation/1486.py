6f57303312b3137960ed7f1b19264a58fda2009b
danbo06 <tanmasaki@gmail.com>
2020-10-01 23:00:00 +0900

    add solution

--- 1486.XOR_Operation/1486.py
+++ 1486.XOR_Operation/1486.py
@@ -9,3 +9,13 @@ class Solution:
         for n in nums:
             ans = ans ^ n
         return ans 
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        
+        ans = 0
+        nums = [start + n * 2 for n in range(n)]
+        
+        for n in nums:
+            ans = ans ^ n
+        return ans 
