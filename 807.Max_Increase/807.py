478ed81497a9ee51467bd7cb9544d6628915c83f
danbo06 <tanmasaki@gmail.com>
2020-10-02 20:25:29 +0900

    add 807

---
+++ 807.Max_Increase/807.py
@@ -0,0 +1,10 @@
+https://leetcode.com/problems/max-increase-to-keep-city-skyline/
+
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        row_max = [max(row) for row in grid]
+        col_max = [max(col) for col in zip(*grid)]
+
+        return sum(min(row_max[r], col_max[c]) -val 
+                for r, row in enumerate(grid) 
+                for c, val in enumerate(row))
