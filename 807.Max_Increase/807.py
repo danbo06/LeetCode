72f4319829885fee89dfe0e8d044b1d45c233337
danbo06 <tanmasaki@gmail.com>
2020-10-02 20:29:38 +0900

    fix

--- 807.Max_Increase/807.py
+++ 807.Max_Increase/807.py
@@ -4,7 +4,7 @@ class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
         row_max = [max(row) for row in grid]
         col_max = [max(col) for col in zip(*grid)]
-
+        
         return sum(min(row_max[r], col_max[c]) -val 
                 for r, row in enumerate(grid) 
                 for c, val in enumerate(row))
