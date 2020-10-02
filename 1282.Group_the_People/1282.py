65bd8f0093bcd76c6b0766bede3ef5e70da8b2ed
danbo06 <tanmasaki@gmail.com>
2020-10-02 09:08:36 +0900

    add HashMap

--- 1282.Group_the_People/1282.py
+++ 1282.Group_the_People/1282.py
@@ -9,3 +9,10 @@ class Solution:
                 ans += [cnt[n]]
                 cnt[n] = []
         return ans
+
+# HashMap
+        def groupThePeople(self, groupSizes):
+        count = collections.defaultdict(list)
+        for i, size in enumerate(groupSizes):
+            count[size].append(i)
+        return [l[i:i + s]for s, l in count.items() for i in xrange(0, len(l), s)]
