b2441314f8f18200eb5d62770f6bdf9e45ddabee
danbo06 <tanmasaki@gmail.com>
2020-10-05 20:06:47 +0900

    add solution

--- 1221.Split_a_String/1221.py
+++ 1221.Split_a_String/1221.py
@@ -2,4 +2,13 @@ https://leetcode.com/problems/split-a-string-in-balanced-strings/
 
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        
+        ans = 0
+        cnt = 0
+        for c in s:
+            if c == 'L':
+                cnt += 1
+            else:
+                cnt -= 1
+            if cnt == 0:
+                ans += 1
+        return ans
