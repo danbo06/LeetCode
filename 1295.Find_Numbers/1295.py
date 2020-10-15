b034902267529110840d078e1aaebe4e8197f86f
danbo06 <tanmasaki@gmail.com>
2020-10-15 20:51:41 +0900

    add 1295

---
+++ 1295.Find_Numbers/1295.py
@@ -0,0 +1,9 @@
+https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        ans = 0
+        for num in nums:
+            if len(str(num)) % 2 == 0:
+                ans += 1
+        return ans
