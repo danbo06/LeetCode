4dd58fe4695f4b3016b38c77b4de0e62cb3842e0
danbo06 <tanmasaki@gmail.com>
2020-10-24 09:52:43 +0900

    add 1323

---
+++ 1323.Maximum_69_Number/1323.py
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/maximum-69-number/
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        ans = 0
+        list_num = str(num)
+        list_num = [s for s in list_num]
+
+        for i in range(len(list_num)):
+            if list_num[i] == '6':
+                list_num[i] = '9'
+                break
+        
+        return "".join(list_num)
