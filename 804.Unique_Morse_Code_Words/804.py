4235ee35e1dd232bee03ff34d9761b46a3c7c301
danbo06 <tanmasaki@gmail.com>
2020-10-28 22:46:36 +0900

    add 804

---
+++ 804.Unique_Morse_Code_Words/804.py
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/unique-morse-code-words/
+
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        MORSE = [".-","-...","-.-.","-..",".","..-.","--.",
+                 "....","..",".---","-.-",".-..","--","-.",
+                 "---",".--.","--.-",".-.","...","-","..-",
+                 "...-",".--","-..-","-.--","--.."]
+
+        seen = {"".join(MORSE[ord(c) - ord('a')] for c in word)
+                for word in words}
+
+        return len(seen)
