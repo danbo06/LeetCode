e0312ed6aaafb05288398488131c95319c031581
danbo06 <tanmasaki@gmail.com>
2020-10-31 09:00:04 +0900

    add 1309

---
+++ 1309.Decrypt_String/1309.py
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
+
+
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        for i in range(26, 0, -1): 
+            s = s.replace(str(i)+'#'*(i>9), chr(96+i))
+        return s
+
+
+
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        for i in range(26,0,-1): s = s.replace(str(i)+'#'*(i>9),chr(96+i))
+        return s
