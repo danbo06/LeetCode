df2c64f1557eed57105a7679d0ed31661cc89405
danbo06 <tanmasaki@gmail.com>
2020-11-14 20:48:39 +0900

    add 1640

---
+++ 1640.Check_Array_Formation/1640.py
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/check-array-formation-through-concatenation/
+
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        mp = {x[0]: x for x in pieces}
+        res = []
+        
+        for num in arr:
+            res += mp.get(num, [])
+            
+        return res == arr
